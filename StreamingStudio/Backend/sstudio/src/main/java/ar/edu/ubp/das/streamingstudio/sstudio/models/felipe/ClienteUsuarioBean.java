7b30eb78db001ae0adf993b2c7e1281689bf8750
felipeBozzano <62155787+felipeBozzano@users.noreply.github.com>
2024-03-24 19:01:36 -0300

    seguimos backend

---
+++ StreamingStudio/Backend/sstudio/src/main/java/ar/edu/ubp/das/streamingstudio/sstudio/models/felipe/ClienteUsuarioBean.java
@@ -0,0 +1,15 @@
+package ar.edu.ubp.das.streamingstudio.sstudio.models.felipe;
+
+public class ClienteUsuarioBean {
+
+    private int id_cliente;
+    private String usuario;
+    private String contrasena;
+    private String email;
+    private String nombre;
+
+
+    ClienteUsuarioBean(int id_cliente, String usuario, String contrasena, String email, String nombre, String apellido, Boolean valido) {
+
+    }
+}
