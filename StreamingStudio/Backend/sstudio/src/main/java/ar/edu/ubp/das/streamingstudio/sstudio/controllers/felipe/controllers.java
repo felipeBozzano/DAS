f75963d54ca61721bafb88ce0b9757ab5ad3e455
Francisco Augusto Olmos Bossa <folmosbossa@ubp.edu.ar>
2024-04-06 18:59:58 -0300

    added imports

--- StreamingStudio/Backend/sstudio/src/main/java/ar/edu/ubp/das/streamingstudio/sstudio/controllers/felipe/controllers.java
+++ StreamingStudio/Backend/sstudio/src/main/java/ar/edu/ubp/das/streamingstudio/sstudio/controllers/felipe/controllers.java
@@ -1,22 +1,29 @@
-//package ar.edu.ubp.das.streamingstudio.sstudio.controllers.felipe;
-//
-//import ar.edu.ubp.das.streamingstudio.sstudio.repositories.felipe.HomeRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
-//@Controller
-//@RequestMapping(
-//        path = "/ss")
-//
-//public class controllers {
-//
-//    @Autowired
-//    HomeRepository repository;
-//
-////    @GetMapping("/home")
-////    public ResponseEntity<Map<String, List<?>>> getTipoFee(@RequestParam("id_cliente") int id_cliente){
-////        return new ResponseEntity<Map<String, List<?>>>(repository.getHome(id_cliente), HttpStatus.OK);
-////    }
-//}
-//
+package ar.edu.ubp.das.streamingstudio.sstudio.controllers.felipe;
+
+import ar.edu.ubp.das.streamingstudio.sstudio.repositories.felipe.HomeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping(
+        path = "/ss")
+
+public class controllers {
+
+    @Autowired
+    HomeRepository repository;
+
+    @GetMapping("/home")
+    public ResponseEntity<Map<String, List<?>>> getTipoFee(@RequestParam("id_cliente") int id_cliente){
+        return new ResponseEntity<Map<String, List<?>>>(repository.getHome(id_cliente), HttpStatus.OK);
+    }
+}
+
