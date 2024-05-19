077af24e4b36ed91a2caa5ec91dcdb6fb08b36d9
Francisco Augusto Olmos Bossa <folmosbossa@ubp.edu.ar>
2024-05-19 16:00:19 -0300

    estilos

--- StreamingStudio/Frontend/streamingStudio/src/app/main/components/publicidades-abajo/publicidades-abajo.component.ts
+++ StreamingStudio/Frontend/streamingStudio/src/app/main/components/publicidades-abajo/publicidades-abajo.component.ts
@@ -17,16 +17,24 @@ export class PublicidadAbajoComponent {
   public publicidades: Publicidad[] = [];
   public publicidad_abajo_izquierda: any;
   public publicidad_abajo_derecha: any;
+  public publicidades_abajo_izquierda_random: any;
+  public publicidades_abajo_derecha_random: any;
 
   constructor(private publicidadesService: PublicationService){}
 
   processPublicidades(){
     this.publicidad_abajo_izquierda = this.publicidades.filter(pub => pub.id_tipo_banner === 3);
     this.publicidad_abajo_derecha = this.publicidades.filter(pub => pub.id_tipo_banner === 4);
-    console.log("publicidad_abajo_izquierda: ", this.publicidad_abajo_izquierda);
-    console.log("publicidad_abajo_derecha: ", this.publicidad_abajo_derecha);
+    //this.publicidades_abajo_izquierda_random = this.obtenerElementoAleatorio(this.publicidad_abajo_izquierda);
+    //this.publicidades_abajo_derecha_random = this.obtenerElementoAleatorio(this.publicidad_abajo_derecha);
+    //console.log("publicidades_abajo_izquierda_random: ", this.publicidades_abajo_izquierda_random);
+    //console.log("publicidades_abajo_derecha_radom: ", this.publicidades_abajo_derecha_random);
   }
 
+  obtenerElementoAleatorio<T>(array: T[]): T {
+    const indiceAleatorio = Math.floor(Math.random() * array.length);
+    return array[indiceAleatorio];
+  }
   ngOnInit(): void {
     this.publicidades = this.publicidadesService.getCurrenPublications();
     this.processPublicidades()
--- StreamingStudio/Frontend/streamingStudio/src/app/main/components/publicidades-arriba/publicidad-arriba.component.ts
+++ StreamingStudio/Frontend/streamingStudio/src/app/main/components/publicidades-arriba/publicidad-arriba.component.ts
@@ -15,11 +15,8 @@ interface Publicidad {
 })
 export class PublicidadArribaComponent {
   @Input() publicidades: Publicidad[] = [];
-  @Input() tipo: 'exclusiva' | 'no_exclusiva' = 'no_exclusiva';
   public publicidad_arriba_izquierda: any;
   public publicidad_arriba_derecha: any;
-  public publicidad_abajo_izquierda: any;
-  public publicidad_abajo_derecha: any;
   public publicidades_arriba_izquierda_random: any;
   public publicidades_arriba_derecha_radom: any;
 
@@ -29,10 +26,8 @@ export class PublicidadArribaComponent {
     console.log("puublicidades: ", this.publicidades);
     this.publicidad_arriba_izquierda = this.publicidades.filter(pub => pub.id_tipo_banner === 1 || pub.id_tipo_banner === 5);
     this.publicidad_arriba_derecha = this.publicidades.filter(pub => pub.id_tipo_banner === 2 || pub.id_tipo_banner === 6);
-    this.publicidad_abajo_izquierda = this.publicidades.filter(pub => pub.id_tipo_banner === 3);
-    this.publicidad_abajo_derecha = this.publicidades.filter(pub => pub.id_tipo_banner === 4);
-    this.publicidades_arriba_izquierda_random = this.publicidad_arriba_izquierda = this.obtenerElementoAleatorio(this.publicidad_arriba_izquierda);
-    this.publicidades_arriba_derecha_radom = this.publicidad_arriba_izquierda = this.obtenerElementoAleatorio(this.publicidad_arriba_derecha);
+    this.publicidades_arriba_izquierda_random = this.obtenerElementoAleatorio(this.publicidad_arriba_izquierda);
+    this.publicidades_arriba_derecha_radom = this.obtenerElementoAleatorio(this.publicidad_arriba_derecha);
     console.log("publicidades_arriba_izquierda_random: ", this.publicidades_arriba_izquierda_random);
     console.log("publicidades_arriba_derecha_radom: ", this.publicidades_arriba_derecha_radom);
   }
